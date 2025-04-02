498467402fafea6520544339980a8e4750c5cd34
Rodrigo Taveira Janeiro <rtjoa@iscte-iul.pt>
2025-04-02 19:40:39 +0100

    Implementar exibição de grafo em JavaFX usando Graphviz

--- Projeto_ES_2024_25/src/main/java/projeto/es/App.java
+++ Projeto_ES_2024_25/src/main/java/projeto/es/App.java
@@ -1,14 +1,8 @@
 package projeto.es;
-
-import java.util.List;
+import javafx.application.Application;
 
 public class App {
     public static void main(String[] args) {
-        try {
-            List<Propriedade> propriedades = CSVLoader.LoadPropriedades("Madeira");
-            System.out.println(propriedades.get(0).toString());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        Application.launch(ApplicationViewer.class, args);
     }
 }
